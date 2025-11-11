673f1f66e09939603f7139e314a4f0582f1f1454
Zifa <zifa04@mail.ru>
2025-11-11 16:02:56 +0300

    list

--- app/src/main/java/com/example/collections/MainActivity.kt
+++ app/src/main/java/com/example/collections/MainActivity.kt
@@ -20,6 +20,29 @@ fun exampleArrays(){
     println(newSolarSystem[8])
 }
 
+fun exampleLists() {
+    val solarSystem = listOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    println(solarSystem.size)
+    println(solarSystem[2])
+    println(solarSystem.indexOf("Earth"))
+    println(solarSystem.indexOf("Pluto"))
+
+    for (planet in solarSystem) {
+        println(planet)
+    }
+    val mutableSolarSystem = mutableListOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    mutableSolarSystem.add("Pluto")
+    mutableSolarSystem.add(3, "Theia")
+    mutableSolarSystem[3] = "Future Moon"
+    println(mutableSolarSystem[3])
+    println(mutableSolarSystem[9])
+    mutableSolarSystem.removeAt(9)
+    mutableSolarSystem.remove("Future Moon")
+    println(mutableSolarSystem.contains("Pluto"))
+    println("Future Moon" in mutableSolarSystem)
+}
+
 fun main() {
     exampleArrays()
+    exampleLists()
 }
