e0ea90937137c71dcdac79aae8bca219c0d0e1ab
Zifa <zifa04@mail.ru>
2025-11-11 16:01:14 +0300

    array

--- app/src/main/java/com/example/collections/MainActivity.kt
+++ app/src/main/java/com/example/collections/MainActivity.kt
@@ -1,5 +1,25 @@
 package com.example.collections
 
-fun main() {
+fun exampleArrays(){
+    val rockPlanets = arrayOf("Mercury", "Venus", "Earth", "Mars")
+    val gasPlanets = arrayOf("Jupiter", "Saturn", "Uranus", "Neptune")
+    val solarSystem = rockPlanets + gasPlanets
+    println(solarSystem[0])
+    println(solarSystem[1])
+    println(solarSystem[2])
+    println(solarSystem[3])
+    println(solarSystem[4])
+    println(solarSystem[5])
+    println(solarSystem[6])
+    println(solarSystem[7])
 
+    solarSystem[3] = "Little Earth"
+    println(solarSystem[3])
+
+    val newSolarSystem = arrayOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto")
+    println(newSolarSystem[8])
+}
+
+fun main() {
+    exampleArrays()
 }
