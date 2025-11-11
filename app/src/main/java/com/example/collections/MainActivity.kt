033b17b0b731e453b142f862879145559055355a
Zifa <zifa04@mail.ru>
2025-11-11 16:03:41 +0300

    map

--- app/src/main/java/com/example/collections/MainActivity.kt
+++ app/src/main/java/com/example/collections/MainActivity.kt
@@ -60,8 +60,34 @@ fun exampleSets() {
     println(solarSystem.contains("Pluto"))
 }
 
+fun exampleMaps() {
+    val solarSystem = mutableMapOf(
+        "Mercury" to 0,
+        "Venus" to 0,
+        "Earth" to 1,
+        "Mars" to 2,
+        "Jupiter" to 79,
+        "Saturn" to 82,
+        "Uranus" to 27,
+        "Neptune" to 14
+    )
+    println(solarSystem.size)
+
+    solarSystem["Pluto"] = 5
+    println(solarSystem.size)
+
+    println(solarSystem["Pluto"])
+
+    solarSystem.remove("Pluto")
+    println(solarSystem.size)
+
+    solarSystem["Jupiter"] = 78
+    println(solarSystem["Jupiter"])
+}
+
 fun main() {
     exampleArrays()
     exampleLists()
     exampleSets()
+    exampleMaps()
 }
